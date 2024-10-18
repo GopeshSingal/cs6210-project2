369198641754ac6aba8a234156e1e4c979e4e457
GopeshSingal <gopesh.singal.03@gmail.com>
2024-10-17 20:10:16 -0400

    Wrote the basic functions for managing a circularly linked list queue

--- src/tinyfile_service.h
+++ src/tinyfile_service.h
@@ -1,12 +1,66 @@
 #ifndef __TINYFILE_SERVICE_H
 #define __TINYFILE_SERVICE_H
 
+#include <stdlib.h>
+
 #define NUM_THREADS 5
 #define MAX_THREADS 8
+
 typedef struct segment {
     int msg_id;
     int seg_id;
 } segment_t;
+
+struct qnode {
+    int recv_id;
+    struct qnode* next;
+};
+
+struct client_queue {
+    struct qnode* front;
+    struct qnode* rear;
+};
+
+struct qnode* create_node(int id) {
+    struct qnode* new_node = (struct qnode*)(malloc(sizeof(struct qnode)));
+    new_node->recv_id = id;
+    new_node->next = NULL;
+    return new_node;
+}
+
+struct client_queue* create_queue() {
+    struct client_queue* queue = (struct client_queue*) malloc(sizeof(struct client_queue));
+    queue->front = queue->rear = NULL;
+    return queue;
+}
+
+void enqueue(struct client_queue* queue, int id) {
+    struct qnode* node = create_node(id);
+    if (queue->rear == NULL) {
+        queue->front = queue->rear = node;
+        queue->rear->next = queue->front;
+    } else {
+        queue->rear->next = node;
+        queue->rear = node;
+        queue->rear->next = queue->front;
+    }
+}
+
+int dequeue(struct client_queue* queue) {
+    int return_id;
+    if (queue->front == queue->rear) {
+        return_id = queue->front->recv_id;
+        free(queue->front);
+        queue->front = queue->rear = NULL;
+    } else {
+        struct qnode* tmp = queue->front;
+        return_id = tmp->recv_id;
+        queue->front = queue->front->next;
+        queue->rear->next = queue->front;
+        free(tmp);
+    }
+    return return_id;
+}
 
 extern int open[MAX_THREADS];
 extern int server_shm_size;
