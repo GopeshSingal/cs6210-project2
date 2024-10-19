35cdc5e90abf90c1a297036cc9ce9a538cbc6c20
glbennett8876 <glbennett8876@gmail.com>
2024-10-18 23:24:58 -0400

    Add ./run.sh for sample app

--- src/run.sh
+++ src/run.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
 
-# Define source and output directories
-# SRC_DIR="../bin/input"
-# OUT_DIR="../bin/output"
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-# rm -r "$OUT_DIR"
+single_file=""
+multiple_files=""
+state="SYNC"
 
-# # Create the output directory if it doesn't exist
-# mkdir -p "$OUT_DIR"
 
-# # Iterate through all input files
-# for file in "$SRC_DIR"/*; do
-#     # Get the filename without path
-#     filename=$(basename -- "$file")
-#     #   echo "${filename%.*}"   --> get filename without extension
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --file)
+      single_file="$2"
+      shift 2
+      ;;
+    --files)
+      multiple_files="$2"
+      shift 2
+      ;;
+    --state)
+      state="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exit 1
+      ;;
+  esac
+done
 
-#     COMPRESSED="$OUT_DIR/compressed_$filename"
-#     UNCOMPRESSED="$OUT_DIR/uncompressed_$filename"
 
-#     ./snappyc_test.o "$file" "$COMPRESSED" "$UNCOMPRESSED"
-# done
+if [ -n "$single_file" ] && ([ -n "$multiple_files" ]); then
+  echo "You can only specify one of --file or --files"
+  exit 1
+fi
 
-./tinyfile_client.o ASYNC ../bin/input/ & 
-./tinyfile_client.o ASYNC ../bin/input/
-# ./tinyfile_client.o ASYNC ../bin/files/
-# ./tinyfile_client.o ASYNC ../bin/files/
-# ./tinyfile_client.o ASYNC ../bin/input/
+# echo "$multiple_files"
+# echo "$state"
+
+if [ -n "$single_file" ]; then
+  ./tinyfile_client.o $state $single_file &
+elif [ -n "$multiple_files" ]; then
+  while IFS= read -r line || [ -n "$line" ]; do
+    ./tinyfile_client.o $state $line 
+  done < "$multiple_files"
+else
+  echo "You must specify either --file or --files"
+  exit 1
+fi
+
